cd73d2a209a9b3bdbeb0e0e1c41d666f96f9ac62
chinhnc <chinhharubn@gmail.com>
2016-10-02 16:37:54 +0700

    add login logout for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: "", path_names: {
+    sign_in: "login", sign_out: "logout",
+    password: "secret", confirmation: "verification",
+    unlock: "unblock", registration: "register", sign_up: "" }
 end
