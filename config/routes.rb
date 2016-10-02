9e673f85d21aa3f9fec369f43911dcb06258434e
duccm94 <minhduc.chu.94@gmail.com>
2016-10-02 22:20:12 +0700

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+  get "help" => "static_pages#help"
+  get "about" => "static_pages#about"
+  get "contact" => "static_pages#contact"
+
   devise_for :users, path: "", path_names: {
     sign_in: "login", sign_out: "logout",
     password: "secret", confirmation: "verification",
